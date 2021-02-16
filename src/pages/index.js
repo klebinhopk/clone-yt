234be061185a533c41facdd08174aca196526744
Kleber P <klebinho.pk@hotmail.com>
2021-02-15 21:26:04 -0300

    Configuration MATERIAL UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 export default function Home() {
   return (
     <>
       <h1>clone YT</h1>
+      <Button variant="outlined" color="secondary">
+        Default
+      </Button>
     </>
   );
 }
